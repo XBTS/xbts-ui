bd0ef8a3064299792628466b23e9f74816c953aa
technolog <viktor@allz.su>
2019-03-30 14:55:31 +0300

    acc:create

--- app/components/Account/AccountAssetCreate.jsx
+++ app/components/Account/AccountAssetCreate.jsx
@@ -832,7 +832,10 @@ class AccountAssetCreate extends React.Component {
         );
 
         return (
-            <div className="grid-content app-tables no-padding" ref="appTables">
+            <div
+                className="grid-content app-tables create-asset"
+                ref="appTables"
+            >
                 <div className="content-block small-12">
                     <div className="tabs-container generic-bordered-box">
                         <div className="tabs-header">
