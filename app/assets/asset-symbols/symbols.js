84f24f8ed8024d8750ef128706e34c244f2ded9b
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2025-05-08 02:21:41 +0300

    add staking information for XBTS in multiple language locales

--- app/assets/asset-symbols/symbols.js
+++ app/assets/asset-symbols/symbols.js
@@ -21,6 +21,7 @@ require("file-loader?name=asset-symbols/[name].png!./pirate.bcc.png");
 require("file-loader?name=asset-symbols/[name].png!./pirate.doge.png");
 require("file-loader?name=asset-symbols/[name].png!./pirate.pirate.png");
 // 3rd party assets
+require("file-loader?name=asset-symbols/[name].png!./defi.png");
 require("file-loader?name=asset-symbols/[name].png!./eth.png");
 require("file-loader?name=asset-symbols/[name].png!./steem.png");
 require("file-loader?name=asset-symbols/[name].png!./mkr.png");
